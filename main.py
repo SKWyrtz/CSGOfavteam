f3b8115e18a6d34190b491243c5f8bfaca43fc52
Sigurd K. Würtz <201906361@post.au.dk>
2021-07-11 17:57:13 +0200

    Added argparse with no of teams

--- main.py
+++ main.py
@@ -2,28 +2,45 @@ import itertools
 import tkinter as tk
 from tkinter import font
 import os
+import argparse
 
-text_file = open("teams(short).txt",  "r")
-lines = text_file.read()
-lines = lines.splitlines()
-teams_dict = dict.fromkeys(lines, 0)
-rows = len(teams_dict)+3
-combinations = list(itertools.combinations(teams_dict, 2))
 comparisons_done = 0
-
 bg_colour ='#0e1f2e'
 accent_colour = '#073957'
 
-combinations_lenght = 0
-for i in combinations:
-    combinations_lenght += 1
-print(f"length of combinations: {combinations_lenght}")
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-t",
+    "--teams",
+    default="20",
+    type=int,
+    help="how many teams to be included (as INT)"
+)
+args = parser.parse_args()
+
+def import_data():
+    global teams_dict, wanted_rows, combinations, combinations_lenght
+
+    text_file = open("teams.txt",  "r")
+    lines = text_file.read()
+    lines = lines.splitlines()
+    teams_dict = dict.fromkeys(lines[0:args.teams], 0)
+
+    wanted_rows = len(teams_dict)+3
+
+    combinations = list(itertools.combinations(teams_dict, 2))
+
+    combinations_lenght = 0
+    for i in combinations:
+        combinations_lenght += 1
+    print(f"length of combinations: {combinations_lenght}")
+
 
 def generate_GUI(root):
     global instructions, team1_label, team2_label, team1_btn, team2_btn, comparisons_left_text
 
     canvas = tk.Canvas(root, width=800, height=300, bg=bg_colour)
-    canvas.grid(columnspan=2, rowspan=rows)
+    canvas.grid(columnspan=2, rowspan=wanted_rows)
 
     #Instructions background
     frame = tk.Frame(root, height=75, width=800, bg=accent_colour)
@@ -71,7 +88,7 @@ def generate_GUI(root):
 
     #Team1 label
     comparisons_left_text = tk.Label(root, text=f"Comparisons left: {combinations_lenght}", font=("Raleway", 18), fg='white', bg=bg_colour)
-    comparisons_left_text.place(x=400, y=250, anchor='center')
+    comparisons_left_text.place(x=290, y=250)
 
 def favorite_selected(team_selected, team_label1, team_label2): 
     global comparisons_done
@@ -83,11 +100,8 @@ def favorite_selected(team_selected, team_label1, team_label2):
 def update_gui(team_selected, team_label1, team_label2):
         global comparisons_done
 
-        print(f"Comparisons done are now: {comparisons_done}")
         team = combinations[comparisons_done][team_selected-1]
-        print(f"The team selected is: {team}")
         teams_dict[team] += 1
-        print("This team has been selected  times")
         if not combinations_lenght-1 == comparisons_done:
             next_team1 = combinations[comparisons_done + 1][0]
             next_team2 = combinations[comparisons_done + 1][1]
@@ -109,26 +123,25 @@ def show_result():
             team_label =tk.Label(root, text=element)
             team_label.grid(columnspan=3, column=0, row=index+1)
             print(element)
-            if index == len(sorted_teams)-1:
-                print("Works")
-                #Export Result Button
-                export_res_btn_text = tk.StringVar()
-                export_res_btn_text.set("Export Result")
-                export_res_btn = tk.Button(root, 
-                                        textvariable=export_res_btn_text,
-                                        font=("Raleway"),
-                                        command=lambda:export_result())
-                export_res_btn.place(x=75, y=270, anchor='center')
+                
+        #Export Result Button
+        export_res_btn_text = tk.StringVar()
+        export_res_btn_text.set("Export Result")
+        export_res_btn = tk.Button(root, 
+                                textvariable=export_res_btn_text,
+                                font=("Raleway", 8),
+                                command=lambda:export_result())
+        export_res_btn.place(x=75, y=270, anchor='center')
 
 def export_result():
-    try:
-        f = open()
+    print("Export")
 
 def restart():
     root.destroy()
     os.startfile("main.py")
     
 if __name__ == '__main__':
+    import_data()
     root = tk.Tk()
     generate_GUI(root)
     root.mainloop()
